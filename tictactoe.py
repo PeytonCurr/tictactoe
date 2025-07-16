670ba58eeb4c916fc48c15fb87565c3510d08a4b
PeytonCurr <peyton.curr@gmail.com>
2025-07-16 11:29:01 -0600

    "==" compares values rather than the object itself like "is"

--- tictactoe.py
+++ tictactoe.py
@@ -26,7 +26,7 @@ def player(board):
     turns = 0
     for row in board:
         for i in range(len(row)):
-            if row[i] is not None:
+            if row[i] != None:
                 turns += 1
     if turns % 2 == 0 and turns > 0:
         return O
@@ -41,7 +41,7 @@ def actions(board):
     actions = []
     for i, row in enumerate(board):
         for j in range(len(row)):
-            if row[j] is None:
+            if row[j] == None:
                 actions.append((i,j))
     return actions
 
@@ -51,7 +51,7 @@ def result(board, action):
     Returns the board that results from making move (i, j) on the board.
     """
     newBoard = copy.deepcopy(board)
-    if board[action[0]][action[1]] is None:
+    if board[action[0]][action[1]] == None:
         newBoard[action[0]][action[1]] = player(board)
         return newBoard
     else:
@@ -64,11 +64,11 @@ def winner(board):
     Returns the winner of the game, if there is one.
     """
     for i in range(3):
-        if board[i][0] == board[i][1] and board[i][1] == board[i][2] and board[i][0] is not None:
+        if board[i][0] == board[i][1] and board[i][1] == board[i][2] and board[i][0] != None:
             return board[i][0]
-        if board[0][i] == board[1][i] and board[1][i] == board[2][i] and board[0][i] is not None:
+        if board[0][i] == board[1][i] and board[1][i] == board[2][i] and board[0][i] != None:
             return board[0][i]
-        if board[0][0] == board[1][1] and board[1][1] == board[2][2] or board[0][2] == board[1][1] and board[1][1] == board[2][0] and board[1][1] is not None:
+        if board[0][0] == board[1][1] and board[1][1] == board[2][2] or board[0][2] == board[1][1] and board[1][1] == board[2][0] and board[1][1] != None:
             return board[1][1]
     
     return None
@@ -79,10 +79,10 @@ def terminal(board):
     Returns True if game is over, False otherwise.
     """
     
-    if winner(board) is None:
+    if winner(board) == None:
         for row in board:
             for i in range(len(row)):
-                if row[i] is None:
+                if row[i] == None:
                     return False
     return True
 
@@ -91,7 +91,8 @@ def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    if winner(board) == X:
+        
 
 
 def minimax(board):
