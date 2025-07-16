ac4796608096cfe5f5f0e638da6c7ff116f22f77
PeytonCurr <peyton.curr@gmail.com>
2025-07-15 21:07:30 -0600

    Implemented Actions function

--- tictactoe.py
+++ tictactoe.py
@@ -63,7 +63,15 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    for i in range(3):
+        if board[i][0] == board[i][1] and board[i][1] == board[i][2] and board[i][0] is not None:
+            return board[i][0]
+        if board[0][i] == board[1][i] and board[1][i] == board[2][i] and board[0][i] is not None:
+            return board[0][i]
+        if board[0][0] == board[1][1] and board[1][1] == board[2][2] or board[0][2] == board[1][1] and board[1][1] == board[2][0] and board[1][1] is not None:
+            return board[1][1]
+    
+    return None
 
 
 def terminal(board):
